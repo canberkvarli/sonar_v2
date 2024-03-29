b95cc38b5a75a7a0107f4163f00e71a66898ecff
Canberk Varli <canberkvarli@gmail.com>
2024-03-28 23:23:45 -0700

    rename homepage

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../components/Home";
+import Homepage from "../components/Homepage";
 
 export default (
     <Router>
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Homepage />} />
         </Routes>
     </Router>
 );
