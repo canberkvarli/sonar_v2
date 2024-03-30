e00b7076d11a31ab40ac40aa94ae04a8296543bf
Canberk Varli <canberkvarli@gmail.com>
2024-03-29 18:33:13 -0700

    add faker users data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'aws-sdk-s3'
+
+p 'Cleaning...Seeding...'
+User.destroy_all
+# Track.destroy_all
+# Like.destroy_all
+
+p 'Creating users...'
+
+10.times do
+  User.create!({
+                 username: Faker::Internet.unique.username,
+                 password_digest: 'password',
+                 artist_name: Faker::Music::PearlJam.unique.musician,
+                 age: rand(18..35),
+                 bio: Faker::Lorem.paragraph,
+                 city: Faker::Address.city,
+                 country: Faker::Address.country,
+                 first_name: Faker::Name.first_name,
+                 last_name: Faker::Name.last_name
+               })
+end
+
+# "demouser" is a special user that can be used to log in to the application.
+User.create!({
+               username: 'demouser',
+               password: 'password'
+             })
+
+p 'Users created.'
