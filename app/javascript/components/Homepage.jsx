4190641db439087cff2e1f5273100ca5766608a8
Canberk Varli <canberkvarli@gmail.com>
2024-03-30 21:44:37 -0700

    add reactbootstrap component library and scss files

--- app/javascript/components/Homepage.jsx
+++ app/javascript/components/Homepage.jsx
@@ -1,8 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Button } from 'react-bootstrap';
 
-export default () => (
-    <div>
-        Hello from React!
-    </div>
-);
+function Homepage() {
+    return (
+        <>
+            <Button variant="primary">Primary</Button>{' '}
+            <Button variant="secondary">Secondary</Button>{' '}
+            <Button variant="success">Success</Button>{' '}
+            <Button variant="warning">Warning</Button>{' '}
+            <Button variant="danger">Danger</Button>{' '}
+            <Button variant="info">Info</Button>{' '}
+            <Button variant="light">Light</Button>{' '}
+            <Button variant="dark">Dark</Button>
+            <Button variant="link">Link</Button>
+        </>
+    );
+}
+
+export default Homepage;
