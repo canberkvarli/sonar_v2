6262d994871421aad0e49daa5108f3c8b6d4244d
Canberk Varli <canberkvarli@gmail.com>
2024-03-30 21:21:08 -0700

    add session login and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,77 @@
 class ApplicationController < ActionController::Base
+  # CRRULLL
+  protect_from_forgery with: :exception
+  before_action :underscore_params!
+
+  skip_before_action :verify_authenticity_token
+  helper_method :current_user, :require_logged_in, :logged_in?
+
+  private
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
+
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
+  def cors_preflight_check
+    return unless request.method == :options
+
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    render text: '', content_type: 'text/plain'
+  end
+
+  def underscore_params!
+    underscore_hash = lambda do |hash|
+      hash.transform_keys!(&:underscore)
+      hash.each do |key, value|
+        if value.is_a?(ActionController::Parameters)
+          underscore_hash.call(value)
+        elsif value.is_a?(Array)
+          value.each do |item|
+            next unless item.is_a?(ActionController::Parameters)
+
+            underscore_hash.call(item)
+          end
+        end
+      end
+    end
+    underscore_hash.call(params)
+  end
+
+  def current_user
+    # find the user by their session token in the cookie
+    return nil unless session[:session_token]
+
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_logged_in
+    return if current_user
+
+    render json: { base: ['invalid credentials'] }, status: 401
+  end
+
+  def login(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    @current_user = user
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    current_user = nil
+  end
 end
