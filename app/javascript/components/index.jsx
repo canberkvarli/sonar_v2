81a7c8f346e7da7c7fe05b8c50a93aa7e32e4be2
Canberk Varli <canberkvarli@gmail.com>
2024-04-03 16:43:42 -0700

    add session control if user logged in

--- app/javascript/components/index.jsx
+++ app/javascript/components/index.jsx
@@ -2,16 +2,37 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux"; // Import Provider
 import configureStore from "../store/store"
+import { fetchTracks } from '../actions/track_actions'
+
 import App from "./App";
 
 document.addEventListener("turbo:load", () => {
-    const store = configureStore();
+    let store;
     const root = createRoot(
         document.body.appendChild(document.createElement("div"))
     );
+
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+
     root.render(
         <Provider store={store}>
             <App />
         </Provider>
     );
+
+    //TESTING
+    window.getState = store.getState;
+    window.fetchTracks = fetchTracks;
+    //TESTING
 });
