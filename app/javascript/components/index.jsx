3d2cd6bb19ddba633d95c2c8960b547719e690d0
Canberk Varli <canberkvarli@gmail.com>
2024-04-03 13:40:59 -0700

    add store provider to App comp

--- app/javascript/components/index.jsx
+++ app/javascript/components/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { Provider } from 'react-redux';
-import store from '../store/store'
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux"; // Import Provider
+import configureStore from "../store/store"
 import App from "./App";
 
 document.addEventListener("turbo:load", () => {
+    const store = configureStore();
     const root = createRoot(
         document.body.appendChild(document.createElement("div"))
-    ); 
-    root.render(<App />);
+    );
+    root.render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
 });
--- app/javascript/store/store.js
+++ app/javascript/store/store.js
@@ -1,19 +1,8 @@
-// import { leagecy_createstore as createStore, applyMiddleware } from "redux";
-
-// import RootReducer from "../reducers/root_reducer";
-// import logger from "redux-logger";
-// import thunk from "redux-thunk";
-// const configureStore = (preloadedState = {}) =>
-//   createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger));
-
-// export default configureStore;
-
-// // LEGACY
 import { createStore, applyMiddleware } from "redux";
 
 import RootReducer from "../reducers/root_reducer";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 
 const configureStore = (preloadedState = {}) =>
   createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger));
