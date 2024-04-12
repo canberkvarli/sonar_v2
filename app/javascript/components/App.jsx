a7efd33ad2f359de94ac952f83120fbb0b53487d
Canberk Varli <canberkvarli@gmail.com>
2024-04-11 17:49:03 -0700

    fix the authroute but protected route is idle

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,25 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Splash from "../components/splash/splash";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HeaderContainer from "./header/header_container";
 import TrackIndexContainer from "../components/tracks/track_index_container";
 import LoginFormContainer from "../components/session_form/login_form_container";
 import SignupFormContainer from "../components/session_form/signup_form_container";
-import { AuthRoute } from "../util/route_util";
 
-export default () => (
-    <Router>
-        <Routes>
-            <Route
-                index element={<HomePage />}
-            />
-            <Route path="/login" element={<LoginFormContainer />} />
-            <Route path="/signup" element={<SignupFormContainer />} />
-        </Routes>
-    </Router>
-);
+export default () => {
+    const loggedIn = useSelector((state) => Boolean(state.session.id));
 
-
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={loggedIn ? <Navigate to="/" /> : <LoginFormContainer />} />
+                <Route path="/signup" element={loggedIn ? <Navigate to="/" /> : <SignupFormContainer />} />
+            </Routes>
+        </Router>
+    );
+};
 
 const HomePage = () => {
     return (
@@ -28,5 +28,5 @@ const HomePage = () => {
             <TrackIndexContainer />
             <Splash />
         </>
-    )
-}
+    );
+};
--- app/javascript/util/route_util.js
+++ app/javascript/util/route_util.js
@@ -1,29 +1,29 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Route, redirect } from "react-router-dom";
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import { Route, redirect } from "react-router-dom";
 
-export const AuthRoute = ({ component: Component, path, exact }) => {
-  const loggedIn = useSelector((state) => Boolean(state.session.id));
+// export const AuthRoute = ({ component: Component, path, exact }) => {
+//   const loggedIn = useSelector((state) => Boolean(state.session.id));
 
-  return (
-    <Route
-      path={path}
-      exact={exact}
-      render={(props) => (!loggedIn ? <Component {...props} /> : redirect("/"))}
-    />
-  );
-};
+//   return (
+//     <Route
+//       path={path}
+//       exact={exact}
+//       render={(props) => (!loggedIn ? <Component {...props} /> : redirect("/"))}
+//     />
+//   );
+// };
 
-export const ProtectedRoute = ({ component: Component, path, exact }) => {
-  const loggedIn = useSelector((state) => Boolean(state.session.id));
+// export const ProtectedRoute = ({ component: Component, path, exact }) => {
+//   const loggedIn = useSelector((state) => Boolean(state.session.id));
 
-  return (
-    <Route
-      path={path}
-      exact={exact}
-      render={(props) =>
-        loggedIn ? <Component {...props} /> : redirect("/login")
-      }
-    />
-  );
-};
+//   return (
+//     <Route
+//       path={path}
+//       exact={exact}
+//       render={(props) =>
+//         loggedIn ? <Component {...props} /> : redirect("/login")
+//       }
+//     />
+//   );
+// };
