b921093b5fe9c18a33f8ba0686ec5657f37439f2
Canberk Varli <canberkvarli@gmail.com>
2024-04-19 12:44:21 -0700

    fix user likes

--- app/javascript/reducers/users_reducer.js
+++ app/javascript/reducers/users_reducer.js
@@ -1,15 +1,31 @@
 import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
-import { RECEIVE_USERS } from "../actions/user_actions";
+import { RECEIVE_USERS, RECEIVE_USER } from "../actions/user_actions";
 
 const usersReducer = (oldState = {}, action) => {
   Object.freeze(oldState);
+
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return Object.assign({}, oldState, {
+      return {
+        ...oldState,
         [action.currentUser.id]: action.currentUser,
-      });
+      };
+
     case RECEIVE_USERS:
-      return Object.assign({}, oldState, action.users);
+      return {
+        ...oldState,
+        ...action.users,
+      };
+
+    case RECEIVE_USER:
+      return {
+        ...oldState,
+        [action.user.id]: {
+          ...oldState[action.user.id],
+          ...action.user,
+        },
+      };
+
     default:
       return oldState;
   }
