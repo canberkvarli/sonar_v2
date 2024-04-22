57e609d97fedcebdf0e1b9d38693649335768414
Canberk Varli <canberkvarli@gmail.com>
2024-04-21 21:33:47 -0700

    change build file

--- bin/sonar_build.sh
+++ bin/sonar_build.sh
@@ -2,9 +2,7 @@
 # exit on error
 set -o errexit
 
-# Add build commands for front end
-npm install && npm run build
-
 bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rails db:seed
