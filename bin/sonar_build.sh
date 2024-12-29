b6e65e3a25c330e970d4b828b0d2692742bad5df
Canberk Varli <canberkvarli@gmail.com>
2024-12-28 21:30:08 -0800

    new render config and db setup!

--- bin/sonar_build.sh
+++ bin/sonar_build.sh
@@ -3,7 +3,12 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
-bundle exec rake db:seed
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
+
+# bundle install
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
+# bundle exec rake db:migrate
+# bundle exec rake db:seed
